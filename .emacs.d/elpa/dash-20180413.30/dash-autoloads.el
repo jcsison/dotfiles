61e8636d318c98b0e96fa3d0b8bc0f9879417d9e
jcsison <jesser.sison@live.com>
2018-04-25 22:30:36 -0700

    [04/25/18 22:30] Added Emacs config

---
+++ .emacs.d/elpa/dash-20180413.30/dash-autoloads.el
@@ -0,0 +1,15 @@
+;;; dash-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil nil ("dash.el") (23265 25119 198625 98000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; dash-autoloads.el ends here
---
+++ .emacs.d/elpa/hl-todo-20180327.1716/hl-todo-pkg.el
@@ -0,0 +1,1 @@
+(define-package "hl-todo" "20180327.1716" "highlight TODO and similar keywords" 'nil :commit "54d6562ba33182b16a7225c5fe6bb17a68025302" :keywords '("convenience") :url "https://github.com/tarsius/hl-todo")
